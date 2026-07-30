c599fcfdce63652cc4746617904bbbd9e80b6072
Kesley <76696517+KesleyExe@users.noreply.github.com>
2026-07-30 14:05:32 -0400

    Redirect root domain to '/app'
    Added a redirect from the root domain to '/app' to prevent 'Cannot GET /' error.

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const io = new Server(server, {
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Redirect root domain "/" to "/app" to prevent "Cannot GET /" on Render
+app.get("/", (req, res) => {
+  res.redirect("/app");
+});
+
 app.get("/app", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "app.html"));
 });
